473574b0af906c1047c593e28473cecf0d79bb4a
thu <mingfei.net@gmail.com>
2016-12-24 14:30:28 +0800

    Update

---
+++ app/src/main/java/com/example/administrator/applicationdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.applicationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+                Log.e(TAG, emailString + ", " + passwordString);
+            }
+        });
+    }
+}
